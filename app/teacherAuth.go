e9d6374f8a57a4835d1cbe53a6aef7137817fdde
pAndrey200 <potseluev-2002@mail.ru>
2021-12-19 22:05:01 +0300

    Update teacherAuth.go

--- app/teacherAuth.go
+++ app/teacherAuth.go
@@ -10,7 +10,7 @@ import (
 var TeacherRights = func(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		notAuth := []string{"/api/user/new", "/api/user/login", "/api/student/info"} //List of endpoints that doesn't require auth
+		notAuth := []string{"/api/user/new", "/api/user/login", "/api/student/info", "/api/student/getAllAttendance"} //List of endpoints that doesn't require auth
 		requestPath := r.URL.Path                               //current request path
 
 		//check if request does not need authentication, serve the request if it doesn't need it
